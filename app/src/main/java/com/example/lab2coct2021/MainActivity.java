9b5b2c4981cfa0fca310773d67d818372e0e187d
fairunnasuha <fairunnasuhan1@gmail.com>
2021-11-24 15:35:24 +0800

    Lab2c

---
+++ app/src/main/java/com/example/lab2coct2021/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.lab2coct2021;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        GridView gridView = (GridView) findViewById(R.id.gridview);
+        gridView.setAdapter(new ImageAdapter(this));
+
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                //sending image id to fullimageActivity
+                Intent i = new Intent(getApplicationContext(), FullImageActivity.class);
+                //passing array index
+                i.putExtra("id", position);
+                startActivity(i);
+            }
+        });
+    }
+}
